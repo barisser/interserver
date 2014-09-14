f4c52533dc27aed5cb25c172b7abe2898f49c329
Andrew Barisser <barisser@gmail.com>
2014-09-14 14:52:12 -0700

    k

--- main.py
+++ main.py
@@ -54,12 +54,13 @@ def messages_query(lastn=None):
 @app.route('/messages/queue', methods=['POST']) #WORDS
 def message_queue():
 
-  text=str(request.form['text'])
-  print text
+  jsoninput=json.loads(request.data)
+
+  text=str(jsoninput['text'])
+
   r=addresses.generate_secure_pair()
   public=r['public_address']
   private=r['private_key']
-  text=jsoninput['text']
 
   #CALCULATE COST NOW
   cost=0.002
